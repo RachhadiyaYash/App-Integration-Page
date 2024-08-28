176f262fd28597a54b89dfd3453dac62001dd542
RachhadiyaYash <yashpatel2462002@gmail.com>
2024-08-28 17:42:01 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla velit
+        corporis, sequi asperiores eius error natus nobis ab dolorum.
+        Repellendus officia vitae culpa illum deserunt, et ut nulla alias
+        provident quos rerum! Eos inventore ipsa mollitia sunt illo, quia nam
+        quas. Esse provident, neque nisi ullam, perspiciatis ea id corporis quas
+        veritatis maxime, voluptatum magni ipsa rerum! Quo omnis quis
+        praesentium illo porro mollitia quae reiciendis maxime atque facere!
+        Ullam, aspernatur quod. At ipsam aspernatur natus tempore? Voluptatum
+        repellat reiciendis dolorum, at omnis fugit exercitationem. Ipsam
+        delectus, ullam assumenda error ex neque libero? Ratione assumenda ipsum
+        ex consectetur, at tenetur dolorem quis doloribus vel ducimus tempore
+        beatae laborum, laboriosam architecto repellendus atque, dolores ab
+        sint! Quibusdam, officiis error consectetur nemo tempore expedita
+        inventore quas nulla amet fugiat eos est iste rerum impedit laboriosam
+        maiores provident tenetur asperiores neque nihil ad ducimus, eius itaque
+        dicta. Molestiae nulla nam odit reprehenderit, distinctio, doloremque
+        eligendi modi doloribus laboriosam tenetur adipisci quibusdam error
+        voluptatem. Ratione ipsum animi quisquam? Eos praesentium incidunt hic
+        voluptates delectus ducimus. Provident veritatis alias optio nisi odit
+        impedit ab error harum quibusdam dolores. Et assumenda perferendis
+        voluptatibus eveniet rem blanditiis inventore ipsum nihil debitis, unde
+        veritatis quo voluptates, mollitia animi quasi ab fugiat esse illum
+        quisquam voluptas quia error ea? Quisquam culpa incidunt maxime eos
+        veniam adipisci commodi animi, unde ipsam minus voluptates, ut assumenda
+        voluptate accusantium possimus similique eius hic blanditiis eaque ea,
+        placeat quia reiciendis voluptatem modi. Debitis cum aperiam maiores
+        molestiae officiis illum praesentium quidem consequatur doloremque sit,
+        id laudantium. Cum totam ullam autem a illo numquam rem assumenda dolore
+        nesciunt. Quaerat sit non facere, rem veritatis, ipsam soluta suscipit
+        impedit recusandae qui nemo dignissimos molestias pariatur, temporibus
+        officiis sint error veniam consequuntur excepturi voluptatum perferendis
+        tempora! Numquam, reprehenderit facilis. Praesentium, expedita provident
+        omnis reiciendis doloribus laboriosam!
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
